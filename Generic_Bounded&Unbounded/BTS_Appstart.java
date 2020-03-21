512653c81956891a7666298d943a1b69d0cb05fe
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-03-21 17:21:44 +0900

    Delete BTS_Appstart.java

--- Generic_Bounded&Unbounded/BTS_Appstart.java
+++
@@ -1,8 +0,0 @@
-public class BTS_Appstart {
-	public static void main(String [] args) {
-		Course<RJ> rj = new Course("sksk", 20);
-//		rj.add(new JIN("ekek", "ekek"));//불가....
-		rj.add(new RJ("알제이", "알기역", 30));//담을 수 있음. 담길때는 상한 값 JIN으로 담긴다.
-		rj.showList();
-	}
-}
