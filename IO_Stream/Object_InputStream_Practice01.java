bad14d259eae1d10220275138f813b3cfeb32ae7
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-03-25 12:49:42 +0900

    Update Object_InputStream_Practice01.java

--- IO_Stream/Object_InputStream_Practice01.java
+++ IO_Stream/Object_InputStream_Practice01.java
@@ -1,29 +1,36 @@
-package ÀÔÃâ·Â¿¬½À;
+package ìž…ì¶œë ¥ì—°ìŠµ;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class Object_InputStream_Practice01 {
 
 	public static void main(String[] args) {
-		FileInputStream fis = null; 
+		ObectFile of = null;
 		ObjectInputStream ois = null;
 		
 		try {
-			fis = new FileInputStream("C:\\bigdataprograming\\data.txt");
-			ois = new ObjectInputStream(fis);
+			of = new ObectFile("C:\\bigdataprograming\\data.txt");
+			ois = new ObjectInputStream(new FileInputStream(of.getFile()));
 			
 			double d = ois.readDouble();
 			int i = ois.readInt();
 			char c = ois.readChar();
-			String str = ois.readLine();
+			Object ob = ois.readObject();
 			
 			System.out.println(d);
 			System.out.println(i);
 			System.out.println(c);
-			System.out.println(str);
+			System.out.println(ob);
 		} catch (Exception e) {
-			// TODO: handle exception
+
+		}finally {
+			try {
+				ois.close();
+			} catch (NullPointerException | IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
