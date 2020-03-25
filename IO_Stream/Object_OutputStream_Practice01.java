5e8311ce2a432e2ab7ebfcad4a2ad3fa13e3eacc
moosongsong <50647845+moosongsong@users.noreply.github.com>
2020-03-25 12:49:23 +0900

    Update Object_OutputStream_Practice01.java

--- IO_Stream/Object_OutputStream_Practice01.java
+++ IO_Stream/Object_OutputStream_Practice01.java
@@ -1,25 +1,51 @@
-package ¿‘√‚∑¬ø¨Ω¿;
+package ÏûÖÏ∂úÎ†•Ïó∞Ïäµ;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
+class ObectFile{
+	private File file;
+
+	public ObectFile(String string) {
+		super();
+		this.file = new File(string);
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	@Override
+	public String toString() {
+		return "ObectFile [file=" + file + "]";
+	}
+	
+	
+}
+
 public class Object_OutputStream_Practice01 {
+	
 
 	public static void main(String[] args) {
-		FileOutputStream fos = null; 
+		ObectFile of = null; 
 		ObjectOutputStream oos = null;
 		
 		try {
-			fos=new FileOutputStream("C:\\bigdataprograming\\data.txt");
-			oos = new ObjectOutputStream(fos);
+			of=new ObectFile("C:\\bigdataprograming\\data.txt");
+			oos = new ObjectOutputStream(new FileOutputStream(of.getFile()));
 			
 			oos.writeDouble(3.14);
 			oos.writeInt(300);
 			oos.writeChar('B');
-			oos.writeChars("FREE");
-			
+			oos.writeObject("wkqk");
+			System.out.println("ÌååÏùºÏì∞Í∏∞ ÏôÑÎ£å");
 		} catch (FileNotFoundException e) {
 
 		} catch (IOException e) {
